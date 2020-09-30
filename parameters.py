bb6d6f54c5a2485cda6e8452414f32426a940563
Thais Luca <thais.luca95@gmail.com>
2020-09-30 17:33:03 +0000

    Adicionando arquivo com todos os parâmetros da aplicação

---
+++ parameters.py
@@ -0,0 +1,10 @@
+
+GOOGLE_WORD2VEC_PATH = 'models/word2vec//GoogleNews-vectors-negative300.bin'
+
+# BoostSRL parameters
+NODESIZE = 2
+NUMOFCLAUSES = 8
+MAXTREEDEPTH = 3
+TREES = 10
+SEED = 441773
+REFINE = None
