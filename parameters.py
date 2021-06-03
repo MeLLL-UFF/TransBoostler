b0a682e9273e5b7ec74f74f4df6f5e2c5f962db4
Thais Luca <thais.luca95@gmail.com>
2021-06-03 18:47:53 -0300

    Alterando parâmetros

--- parameters.py
+++ parameters.py
@@ -14,12 +14,12 @@ EMBEDDING_DIMENSION = 300
 METHOD = 'CONCATENATE'
 #METHOD = None
 
-TOP_K = 2
+TOP_K = 1
 
 # Validation parameters
 N_FOLDS = 3
-AMOUNTS = [0.2, 0.4, 0.6, 0.8, 1.0] 
-#AMOUNTS = [1.0] 
+#AMOUNTS = [0.2, 0.4, 0.6, 0.8, 1.0] 
+AMOUNTS = [1.0] 
 
 # BoostSRL parameters
 NODESIZE = 2
