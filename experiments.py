4bdfca7542173268c15c05942813263bae90600d
Thais Luca <thais.luca95@gmail.com>
2021-06-05 13:28:01 -0300

    Removendo experimento com cosseno

--- experiments.py
+++ experiments.py
@@ -2,17 +2,17 @@
 revision_theory = True
 
 setups = [
-        {'model': 'fasttext', 'similarity_metric': 'cosine', 'revision_theory': revision_theory},
+        ##{'model': 'fasttext', 'similarity_metric': 'cosine', 'revision_theory': revision_theory},
         {'model': 'fasttext', 'similarity_metric': 'euclidean', 'revision_theory': revision_theory},
         {'model': 'fasttext', 'similarity_metric': 'softcosine', 'revision_theory': revision_theory},
         {'model': 'fasttext', 'similarity_metric': 'wmd', 'revision_theory': revision_theory},
         ##{'model': 'fasttext', 'similarity_metric': 'relax-wmd', 'revision_theory': revision_theory},
         
-        ##{'model': 'word2vec', 'similarity_metric': 'softcosine', 'revision_theory': revision_theory},
+        #{'model': 'word2vec', 'similarity_metric': 'softcosine', 'revision_theory': revision_theory},
         ##{'model': 'word2vec', 'similarity_metric': 'wmd', 'revision_theory': revision_theory},
         ##{'model': 'word2vec', 'similarity_metric': 'relax-wmd', 'revision_theory': revision_theory},
-        ##{'model': 'word2vec', 'similarity_metric': 'euclidean', 'revision_theory': revision_theory},
-        ##{'model': 'word2vec', 'similarity_metric': 'cosine', 'revision_theory': revision_theory},
+        #{'model': 'word2vec', 'similarity_metric': 'euclidean', 'revision_theory': revision_theory},
+        #{'model': 'word2vec', 'similarity_metric': 'cosine', 'revision_theory': revision_theory},
 
         # COLOCAR NA DISSERTAÇÃO -> RELAX-WMD
         ]
