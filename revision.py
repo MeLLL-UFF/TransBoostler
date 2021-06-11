7938644bbb98d05065acc04d085eda18639cf252
Thais Luca <thais.luca95@gmail.com>
2021-06-11 18:53:02 -0300

    Ajustando variáveis

--- revision.py
+++ revision.py
@@ -142,7 +142,7 @@ class TheoryRevision:
 			utils.print_function(item, experiment_title)
 		utils.print_function('\n', experiment_title)
 
-		model, t_results, learning_time, inference_time = self.train_and_test(background, train_pos, train_neg, train_facts, test_pos, test_neg, test_facts, refine=params.REFINE_FILENAME, transfer=params.TRANSFER_FILENAME)
+		model, t_results, learning_time, inference_time = self.train_and_test(background, train_pos, train_neg, train_facts, test_pos, test_neg, test_facts, refine=params.REFINE_FILENAME, transfer=params.TRANSFER_FILENAME, experiment_title)
 		pl_t_results = copy.deepcopy(t_results)
 
 		structured = []
@@ -252,7 +252,7 @@ class TheoryRevision:
 
 		return best_model_results, total_revision_time, inference_time
 
-	def train_and_test(self, background, train_pos, train_neg, train_facts, test_pos, test_neg, test_facts, refine=None, transfer=None):
+	def train_and_test(self, background, train_pos, train_neg, train_facts, test_pos, test_neg, test_facts, refine=None, transfer=None, experiment_title):
 		'''
 	        Train RDN-B using transfer learning
 	    '''
