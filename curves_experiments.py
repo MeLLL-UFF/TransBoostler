6dfdc5038a70bdca43044c0a160e6f04ec8f95c9
Thais Luca <thais.luca95@gmail.com>
2021-06-29 17:54:47 -0300

    Arrumando código com experimentos

--- curves_experiments.py
+++ curves_experiments.py
@@ -29,7 +29,7 @@ balanced = False
 
 runTransBoostler = True
 runRDNB = False
-learn_from_source = False
+learn_from_source = True
 
 folds_path = 'folds_transfer_experiment'
 
@@ -201,72 +201,11 @@ def main():
         path = os.getcwd() + '/experiments/' + experiment_title
         if not os.path.exists(path):
             os.mkdir(path)
-                
-        if(learn_from_source):
-
-            # Load source dataset
-            src_total_data = datasets.load(source, bk[source], seed=params.SEED)
-            src_data = datasets.load(source, bk[source], target=predicate, balanced=source_balanced, seed=params.SEED)
-
-            # Group and shuffle
-            src_facts = datasets.group_folds(src_data[0])
-            src_pos   = datasets.group_folds(src_data[1])
-            src_neg   = datasets.group_folds(src_data[2])
-                        
-            utils.print_function('Start learning from source dataset\n')
-            
-            utils.print_function('Source train facts examples: {}'.format(len(src_facts)), experiment_title)
-            utils.print_function('Source train pos examples: {}'.format(len(src_pos)), experiment_title)
-            utils.print_function('Source train neg examples: {}\n'.format(len(src_neg)), experiment_title)
-            
-            start = time.time()
-
-            # Learning from source dataset
-            background = boostsrl.modes(bk[source], [predicate], useStdLogicVariables=False, maxTreeDepth=params.MAXTREEDEPTH, nodeSize=params.NODESIZE, numOfClauses=params.NUMOFCLAUSES)
-            model = boostsrl.train(background, src_pos, src_neg, src_facts, trees=params.TREES)
-            
-            end = time.time()
-
-            #TODO: adicionar o tempo corretamente
-            #print('Model training time {}'.format(model.traintime()))
-
-            utils.print_function('Model training time {} \n'.format(end-start), experiment_title)
-
-            utils.print_function('Building refine structure \n', experiment_title)
-
-            # Get all learned trees
-            structured = []
-            for i in range(params.TREES):
-                structured.append(model.get_structured_tree(treenumber=i+1).copy())
-            
-            will = ['WILL Produced-Tree #'+str(i+1)+'\n'+('\n'.join(model.get_will_produced_tree(treenumber=i+1))) for i in range(10)]
-            for w in will:
-                utils.print_function(w, experiment_title)
-            
-            del model
-
-            # Get the list of predicates from source tree          
-            nodes = utils.deep_first_search_nodes(structured, utils.match_bk_source(set(bk[source])))
-            save_pickle_file(nodes, _id, source, target, params.SOURCE_TREE_NODES_FILES)
-            save_pickle_file(structured, _id, source, target, params.STRUCTURED_TREE_NODES_FILES)
-
-            # Get all rules learned by RDN-B
-            refine_structure = utils.get_all_rules_from_tree(structured)
-            utils.write_to_file(refine_structure, params.REFINE_FILENAME)
-            utils.write_to_file(refine_structure, os.getcwd() + '/experiments/{}_{}_{}/{}'.format(_id, source, target, params.REFINE_FILENAME.split('/')[1]))
-
-        else:
-            utils.print_function('Loading pre-trained trees.', experiment_title)
-
-            from shutil import copyfile
-            copyfile(os.getcwd() + '/resources/{}_{}_{}/{}'.format(_id, source, target, params.REFINE_FILENAME.split('/')[1]), os.getcwd() + '/' + params.REFINE_FILENAME)
-            nodes = load_pickle_file(os.getcwd() + '/resources/{}_{}_{}/{}'.format(_id, source, target, params.SOURCE_TREE_NODES_FILES))
-            structured = load_pickle_file(os.getcwd() + '/resources/{}_{}_{}/{}'.format(_id, source, target, params.STRUCTURED_TREE_NODES_FILES))
 
         # Get targets
         sources = [s.replace('.', '').replace('+', '').replace('-', '') for s in set(bk[source]) if s.split('(')[0] != to_predicate and 'recursion_' not in s]
         targets = [t.replace('.', '').replace('+', '').replace('-', '') for t in set(bk[target]) if t.split('(')[0] != to_predicate and 'recursion_' not in t]
-
+    
         for setup in setups: 
             embeddingModel = setup['model'].lower()
             similarityMetric = setup['similarity_metric'].lower()
@@ -305,7 +244,68 @@ def main():
 
             while results['save']['n_runs'] < n_runs:
                 utils.print_function('Run: ' + str(results['save']['n_runs'] + 1), experiment_title)
-            
+                
+                if(learn_from_source):
+
+                    # Load source dataset
+                    src_total_data = datasets.load(source, bk[source], seed=params.SEED)
+                    src_data = datasets.load(source, bk[source], target=predicate, balanced=source_balanced, seed=params.SEED)
+
+                    # Group and shuffle
+                    src_facts = datasets.group_folds(src_data[0])
+                    src_pos   = datasets.group_folds(src_data[1])
+                    src_neg   = datasets.group_folds(src_data[2])
+                                
+                    utils.print_function('Start learning from source dataset\n')
+                    
+                    utils.print_function('Source train facts examples: {}'.format(len(src_facts)), experiment_title)
+                    utils.print_function('Source train pos examples: {}'.format(len(src_pos)), experiment_title)
+                    utils.print_function('Source train neg examples: {}\n'.format(len(src_neg)), experiment_title)
+                    
+                    start = time.time()
+
+                    # Learning from source dataset
+                    background = boostsrl.modes(bk[source], [predicate], useStdLogicVariables=False, maxTreeDepth=params.MAXTREEDEPTH, nodeSize=params.NODESIZE, numOfClauses=params.NUMOFCLAUSES)
+                    model = boostsrl.train(background, src_pos, src_neg, src_facts, trees=params.TREES)
+                    
+                    end = time.time()
+
+                    #TODO: adicionar o tempo corretamente
+                    #print('Model training time {}'.format(model.traintime()))
+
+                    utils.print_function('Model training time {} \n'.format(end-start), experiment_title)
+
+                    utils.print_function('Building refine structure \n', experiment_title)
+
+                    # Get all learned trees
+                    structured = []
+                    for i in range(params.TREES):
+                        structured.append(model.get_structured_tree(treenumber=i+1).copy())
+                    
+                    will = ['WILL Produced-Tree #'+str(i+1)+'\n'+('\n'.join(model.get_will_produced_tree(treenumber=i+1))) for i in range(10)]
+                    for w in will:
+                        utils.print_function(w, experiment_title)
+                    
+                    del model
+
+                    # Get the list of predicates from source tree          
+                    nodes = utils.deep_first_search_nodes(structured, utils.match_bk_source(set(bk[source])))
+                    save_pickle_file(nodes, _id, source, target, params.SOURCE_TREE_NODES_FILES)
+                    save_pickle_file(structured, _id, source, target, params.STRUCTURED_TREE_NODES_FILES)
+
+                    # Get all rules learned by RDN-B
+                    refine_structure = utils.get_all_rules_from_tree(structured)
+                    utils.write_to_file(refine_structure, params.REFINE_FILENAME)
+                    utils.write_to_file(refine_structure, os.getcwd() + '/experiments/{}_{}_{}/{}'.format(_id, source, target, params.REFINE_FILENAME.split('/')[1]))
+
+                else:
+                    utils.print_function('Loading pre-trained trees.', experiment_title)
+
+                    from shutil import copyfile
+                    copyfile(os.getcwd() + '/resources/{}_{}_{}/{}'.format(_id, source, target, params.REFINE_FILENAME.split('/')[1]), os.getcwd() + '/' + params.REFINE_FILENAME)
+                    nodes = load_pickle_file(os.getcwd() + '/resources/{}_{}_{}/{}'.format(_id, source, target, params.SOURCE_TREE_NODES_FILES))
+                    structured = load_pickle_file(os.getcwd() + '/resources/{}_{}_{}/{}'.format(_id, source, target, params.STRUCTURED_TREE_NODES_FILES))
+
                 start = time.time()
 
                 # Map and transfer using the loaded embedding model
